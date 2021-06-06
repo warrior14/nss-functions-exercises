e7f46d3423caf12f51b1ce7ec17ffd952fe372be
warrior14 <ironaddicts14@gmail.com>
2021-06-06 14:48:56 -0500

    my initial commit, functions practice

---
+++ practice.js
@@ -0,0 +1,61 @@
+// Calcaulate age in dog years exercise
+
+// const calculateAgeInDogYears = (ageOfPerson) => {
+//     const ageInDogYears = ageOfPerson / 7
+
+//     return ageInDogYears
+// }
+
+// const dogAge = calculateAgeInDogYears(25)
+// console.log(dogAge)
+
+
+// Calculate age in cat years exercise
+
+// const calculateAgeInCatYears = (ageOfHuman) => {
+//     const ageInCatYears = ageOfHuman / 5
+
+//     return ageInCatYears
+// }
+
+// const catAge = calculateAgeInCatYears(50)
+// console.log(catAge)
+
+// Calculation example
+// const addNumbers = (num1, num2) => {
+//     const result = num1 + num2
+//         return result
+    
+// };
+
+// const results = addNumbers(5,10)
+// console.log(results);
+
+// Practice best in show
+
+// const favoriteBreed = (petBreed) => {
+//     if (petBreed === "meow") {
+//         return "I like cats"
+//     } else {
+//         return `My favorite breed is ${petBreed}`
+//     }
+// }
+
+// const myFavoriteBreed = favoriteBreed('Pitbull')
+// console.log(`When it comes to pets, ${myFavoriteBreed}`);
+// const myFavBreed = favoriteBreed("meow")
+// console.log(myFavBreed);
+
+
+// const favoriteGame = (gameType) => {
+//     if (gameType === "COD") {
+//         return "I like to play COD"
+//     } else {
+//         return `My favorite game is ${gameType}`
+//     }
+// }
+
+// const bestGame = favoriteGame("COD")
+// console.log(bestGame);
+
+// PRACTICE : ADDITION
